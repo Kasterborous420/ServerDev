f4a5ba7bdf78a52ae94eefba301d3832903d29f8
mutha yuckar <yuckar97@gmail.com>
2016-05-30 11:47:38 +0800

    did shit

--- project/server/main/views.py
+++ project/server/main/views.py
@@ -4,11 +4,13 @@
 #################
 #### imports ####
 #################
-
-from flask import Flask, url_for
+import json
+from flask import Flask, url_for, render_template, request, urllib
 app = Flask(__name__)
 
-
+dict['Words'] = response.urllib.urlopen("http://randomword.setgetgo.com/get.php")
+gamesWon = 0
+gamesLost = 0
 ################
 #### config ####
 ################
@@ -25,14 +27,36 @@ app = Flask(__name__)
 ##def login():
 ##    if request.method == 'POST' :
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        return render_template('index.html')
+@app.route('/')
+def hello:
+    return render_template(index.html)
+
+@app.route('/new_game', methods = ['POST'])
+def new_game:
+    if request.method == 'POST':
+
+        dict['word_length'] = len(response.urllib.urlopen("http://randomword.setgetgo.com/get.php"))
+        return json.dump(dict)
+
+@app.route('/check_letter', methods = ['POST'])
+def check_letter:
+    global dict
+    if request.method == 'POST';
+    dict['letter'] = len(dict["Words"])
+
+@app.route('/score', methods = ['GET'])
+def getScore:
+    print "Games Won" % gamesWon
+    print "Games Lost" % gamesLost
+
+@app.route('/score', methods = ['DELETE'])
+def delScore:
+    gamesWon = 0
+    gamesLost = 0
+    print "Games Won" % gamesWon
+    print "Games Lost" % gamesLost
+
+
 
 if __name__ == '__main__':
     app.run()
-
-@main_blueprint.route("/about/")
-def about():
-    return render_template("main/about.html")
